0c4bddacdf887f1ad13fbdb1a5c06413d13ddc6a
xutianzi333 <37890774+xutianzi333@users.noreply.github.com>
2019-11-20 23:50:46 +0800

    Fixed a thread blocking bug. (#6)
    Fixed a thread blocking bug. (#6)

--- src/main/java/com/github/hcsp/Executor.java
+++ src/main/java/com/github/hcsp/Executor.java
@@ -35,8 +35,19 @@ public class Executor {
     // 任务执行期间若抛出任何异常，则在主线程中重新抛出它
     // 请回答：
     // 1. 这个方法运行的流程是怎么样的？运行的时候JVM中有几个线程？它们如何互相交互？
+    // a1. 主线程作为生产者，将tasks放入queue中。消费线程通过while(ture)循环对task进行消费（执行Lambda表达式）;
+    //     当consumer线程吃到毒丸时while(true)循环break，主线程（生产者线程）等待consumer.join()。
+    // a2. 运行的时候JVM有 1(main) + 1(consumer) + Math.min(tasks.size(), numberOfThreads)个线程.
+    // a3. 通过BlockingQueue交互，主线程通过线程池为每一个任务开启一个工作线程执行task并put进queue中;
+    //     consumer执行queue.poll()方法获取并删除头结点，consumer执行Feature.get()获取工作线程执行的结果。
+
     // 2. 为什么有的时候会卡死？应该如何修复？
+    // 在throw new IllegalStateException时异常被catch后break跳出循环，消费者无法继续进行get操作。
+    // 由于queue的容量为nuberOfThreads，生产者的put操作处于长期阻塞中。
+
     // 3. PoisonPill是什么东西？如果不懂的话可以搜索一下。
+    // 毒丸模式用于通知消费者生产者已经完成了生产消息的工作，同时消费者没有多余的任务需要继续等待。
+    // 毒丸代表queue中最后一个事件，执行完毒丸事件，线程终止。
     public static <T> void runInParallelButConsumeInSerial(List<Callable<T>> tasks,
                                                             Consumer<T> consumer,
                                                             int numberOfThreads) throws Exception {
@@ -55,7 +66,7 @@ public class Executor {
                         consumer.accept(future.get());
                     } catch (Exception e) {
                         exceptionInConsumerThread.set(e);
-                        break;
+                        // break;
                     }
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
