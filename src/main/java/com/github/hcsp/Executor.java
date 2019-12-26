8105682cba390f597737fbb4f37459fb45eebde3
kisslonglin <37239582+kisslonglin@users.noreply.github.com>
2019-12-26 17:22:58 +0800

    程序卡死Bug修复 (#11)
    程序卡死Bug修复 (#11)

--- src/main/java/com/github/hcsp/Executor.java
+++ src/main/java/com/github/hcsp/Executor.java
@@ -35,8 +35,15 @@ public class Executor {
     // 任务执行期间若抛出任何异常，则在主线程中重新抛出它
     // 请回答：
     // 1. 这个方法运行的流程是怎么样的？运行的时候JVM中有几个线程？它们如何互相交互？
+    //答：1.首先创建一个Block队列
+     //2.然后创建一个消费者线程去循环消费该阻塞队列
+     //3.创建一个线程池并提交Callable任务，然后放入阻塞队列
+     //4.等待消费者完成任务
     // 2. 为什么有的时候会卡死？应该如何修复？
+    //答：消费者宕机,产生了IllegalStateException异常，
+    // 如何修复：处理掉异常再重新消费 或者在线程执行完成之后处理
     // 3. PoisonPill是什么东西？如果不懂的话可以搜索一下。
+    //答：毒药丸策略，以便在执行完成时知道放入队列的消息类型
     public static <T> void runInParallelButConsumeInSerial(List<Callable<T>> tasks,
                                                             Consumer<T> consumer,
                                                             int numberOfThreads) throws Exception {
@@ -55,7 +62,7 @@ public class Executor {
                         consumer.accept(future.get());
                     } catch (Exception e) {
                         exceptionInConsumerThread.set(e);
-                        break;
+//                        break;
                     }
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
