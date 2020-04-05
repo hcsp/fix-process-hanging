783f968da6d0b8dbd954d48ce9b5c40dd40b8195
hah1346798520 <51789422+hah1346798520@users.noreply.github.com>
2020-04-05 16:58:11 +0800

    修复程序卡死的问题 (#22)
    修复程序卡死的问题 (#22)

--- src/main/java/com/github/hcsp/Executor.java
+++ src/main/java/com/github/hcsp/Executor.java
@@ -2,16 +2,7 @@ package com.github.hcsp;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.*;
 import java.util.function.Consumer;
 
 public class Executor {
@@ -38,11 +29,11 @@ public class Executor {
     // 2. 为什么有的时候会卡死？应该如何修复？
     // 3. PoisonPill是什么东西？如果不懂的话可以搜索一下。
     public static <T> void runInParallelButConsumeInSerial(List<Callable<T>> tasks,
-                                                            Consumer<T> consumer,
-                                                            int numberOfThreads) throws Exception {
+                                                           Consumer<T> consumer,
+                                                           int numberOfThreads) throws Exception {
         BlockingQueue<Future<T>> queue = new LinkedBlockingQueue<>(numberOfThreads);
-        AtomicReference<Exception> exceptionInConsumerThread = new AtomicReference<>();
-
+//        AtomicReference<Exception> exceptionInConsumerThread = new AtomicReference<>();
+        List<Exception> exceptionInConsumerThread = new CopyOnWriteArrayList<>();
         Thread consumerThread = new Thread(() -> {
             while (true) {
                 try {
@@ -54,8 +45,7 @@ public class Executor {
                     try {
                         consumer.accept(future.get());
                     } catch (Exception e) {
-                        exceptionInConsumerThread.set(e);
-                        break;
+                        exceptionInConsumerThread.add(e);
                     }
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
@@ -76,8 +66,12 @@ public class Executor {
 
         threadPool.shutdown();
 
-        if (exceptionInConsumerThread.get() != null) {
-            throw exceptionInConsumerThread.get();
+        if (!exceptionInConsumerThread.isEmpty()) {
+            StringBuilder exceptionMsg = new StringBuilder();
+            for (Exception e : exceptionInConsumerThread) {
+                exceptionMsg.append(e.getMessage());
+            }
+            throw new IllegalStateException(exceptionMsg.toString());
         }
     }
 
