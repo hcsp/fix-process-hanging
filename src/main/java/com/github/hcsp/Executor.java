cf43437c45e0a858c817572ea6bba7ca08cf9824
shenzhenjie <851284547@qq.com>
2020-04-21 22:25:47 +0800

    异常抛出导致没有消费者，阻塞队列占满，卡死问题

--- src/main/java/com/github/hcsp/Executor.java
+++ src/main/java/com/github/hcsp/Executor.java
@@ -1,5 +1,6 @@
 package com.github.hcsp;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
@@ -35,14 +36,23 @@ public class Executor {
     // 任务执行期间若抛出任何异常，则在主线程中重新抛出它
     // 请回答：
     // 1. 这个方法运行的流程是怎么样的？运行的时候JVM中有几个线程？它们如何互相交互？
+    //  主线程=》启动子线程=》启动线程池任务=》添加标志位等待子线程结束=》等待线程池结束
+    //  两个线程一个线程池
+    //  通过blockQueue添加标志位判断是否结束子线程，线程池和子线程竞争，一个put一个poll
     // 2. 为什么有的时候会卡死？应该如何修复？
+    //  阻塞队列大小是固定的，生产者遇到异常break，导致队列满了，消费者线程池put不进去，导致阻塞。
+    //  参照其他同学：
+    //  1）.不让生产者break 而是continue继续工作，记录下异常
+    //  2）.让消费者也就是线程池，在遇到异常时，关闭线程池，停止put
+    //  3）.使用queue.offer() 方法增加超时时间
+    //  4）.不限制队列大小，但是会造成内存泄漏
+    //  5）.满了不再put
     // 3. PoisonPill是什么东西？如果不懂的话可以搜索一下。
     public static <T> void runInParallelButConsumeInSerial(List<Callable<T>> tasks,
                                                             Consumer<T> consumer,
                                                             int numberOfThreads) throws Exception {
         BlockingQueue<Future<T>> queue = new LinkedBlockingQueue<>(numberOfThreads);
-        AtomicReference<Exception> exceptionInConsumerThread = new AtomicReference<>();
-
+        List<Exception> exceptionInConsumerThread = new ArrayList();
         Thread consumerThread = new Thread(() -> {
             while (true) {
                 try {
@@ -54,8 +64,9 @@ public class Executor {
                     try {
                         consumer.accept(future.get());
                     } catch (Exception e) {
-                        exceptionInConsumerThread.set(e);
-                        break;
+                        exceptionInConsumerThread.add(e);
+//                        exceptionInConsumerThread.set(e);
+//                        break;
                     }
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
@@ -76,8 +87,8 @@ public class Executor {
 
         threadPool.shutdown();
 
-        if (exceptionInConsumerThread.get() != null) {
-            throw exceptionInConsumerThread.get();
+        if (exceptionInConsumerThread.size() >0) {
+            System.out.println(exceptionInConsumerThread.size()+"个异常，请进行排查");
         }
     }
 
