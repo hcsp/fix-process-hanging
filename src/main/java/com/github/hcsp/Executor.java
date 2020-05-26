eef924204c77f60c99eb459b32126aa34f4f9910
Suyu <zhuangsuyu1993@163.com>
2020-05-26 15:58:06 +0800

    解决消费者无法消费导致的阻塞队列填满，线程等待的问题 (#27)
    解决消费者无法消费导致的阻塞队列填满，线程等待的问题 (#27)

--- src/main/java/com/github/hcsp/Executor.java
+++ src/main/java/com/github/hcsp/Executor.java
@@ -35,8 +35,22 @@ public class Executor {
     // 任务执行期间若抛出任何异常，则在主线程中重新抛出它
     // 请回答：
     // 1. 这个方法运行的流程是怎么样的？运行的时候JVM中有几个线程？它们如何互相交互？
+    // 定义了定长的阻塞队列LinkedBlockingQueue，
+    // 一共3种线程，main线程（也是生产者），消费线程和执行任务的线程
+    // 执行任务的线程由缓存线程池创建，执行任务调用后，由main线程负责把Future放入阻塞队列
+    // 消费线程会串行地消费阻塞队列里的信息。
+    // 当消费线程遇到毒丸对象的时候，消费停止。
+
     // 2. 为什么有的时候会卡死？应该如何修复？
+    // 在抛出异常的消费方法中，main线程在向阻塞队列中放毒丸对象的过程中卡死（WAITING状态）
+    // 原因是消费线程在消费第一个future时，抛出异常退出了消费线程，导致无法继续消费，阻塞队列扔满了任务
+    // 所以main线程在向阻塞队列中放毒丸对象时发现阻塞队列满了，于是等待消费
+    // 修复方法就是，在消费线程中遇到异常不用break，处理后继续消费下一个
+
     // 3. PoisonPill是什么东西？如果不懂的话可以搜索一下。
+    // “毒丸”对象将确保消费者在关闭之前首先完成队列中的所有任务，
+    // “毒丸”对象之前提交的所有任务都会被处理，
+    // 而生产者在提交了“毒丸”对象后，将不会再提交任何任务。
     public static <T> void runInParallelButConsumeInSerial(List<Callable<T>> tasks,
                                                             Consumer<T> consumer,
                                                             int numberOfThreads) throws Exception {
@@ -55,7 +69,7 @@ public class Executor {
                         consumer.accept(future.get());
                     } catch (Exception e) {
                         exceptionInConsumerThread.set(e);
-                        break;
+                        //break;
                     }
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
